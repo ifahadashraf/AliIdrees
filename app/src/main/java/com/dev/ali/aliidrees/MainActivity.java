b35873262081543f7e55053c6e844cf33b6811e7
ifahadashraf <bcsf14a535@pucit.edu.pk>
2018-05-03 23:42:55 +0500

    init

---
+++ app/src/main/java/com/dev/ali/aliidrees/MainActivity.java
@@ -0,0 +1,23 @@
+package com.dev.ali.aliidrees;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
+}
